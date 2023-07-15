3e1949cf25f3d30574307079477f3b9dfc61edc5
BactSymEvol <46500643+BactSymEvol@users.noreply.github.com>
2023-07-15 10:45:04 -0400

    Update window1.py

--- gui/subclass/window1.py
+++ gui/subclass/window1.py
@@ -51,14 +51,3 @@ def uploaddata():
         return (ui.getCheckedUpload())
     else:
         return ()
-'''
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    sys.exit(app.exec_())
-'''
