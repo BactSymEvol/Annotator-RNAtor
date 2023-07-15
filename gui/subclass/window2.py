2199fee5596e26a143326e37da5e8c21fddd7ef7
BactSymEvol <46500643+BactSymEvol@users.noreply.github.com>
2023-07-15 10:45:22 -0400

    Update window2.py

--- gui/subclass/window2.py
+++ gui/subclass/window2.py
@@ -73,13 +73,4 @@ def automatic_upload():
     else:
         return ()
 
-'''
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    sys.exit(app.exec_())
-'''
+
